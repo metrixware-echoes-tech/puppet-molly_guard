11b97aa1be598826a2c2e1a4c2413a8f42a59454
Florent Poinsaut <florent@echoes.fr>
2017-02-28 16:19:54 +0100

    fix 2 bad module names

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -14,7 +14,7 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module
-    puppet_module_install(:source => module_root, :module_name => 'monit')
+    puppet_module_install(:source => module_root, :module_name => 'molly_guard')
     hosts.each do |host|
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0, 1] }
     end
