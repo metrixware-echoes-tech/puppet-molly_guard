8629b960052dfe651ad02f94803d904d82385758
Florent Poinsaut <florent@poinsaut.fr>
2018-04-12 11:26:25 +0200

    pdk test unit

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,130 +1,72 @@
 require 'spec_helper'
-describe 'molly_guard', type: 'class' do
-  platforms = {
-    'debian6' =>
-      { osfamily: 'Debian',
-        release: '6.0',
-        majrelease: '6',
-        lsbdistcodename: 'squeeze',
-        packages: 'molly_guard' },
-    'debian7' =>
-      { osfamily: 'Debian',
-        release: '7.0',
-        majrelease: '7',
-        lsbdistcodename: 'wheezy',
-        packages: 'molly_guard' },
-    'debian8' =>
-      { osfamily: 'Debian',
-        release: '8.0',
-        majrelease: '8',
-        lsbdistcodename: 'jessie',
-        packages: 'molly_guard' },
-    'ubuntu1004' =>
-      { osfamily: 'Debian',
-        release: '10.04',
-        majrelease: '10',
-        lsbdistcodename: 'lucid',
-        packages: 'molly_guard' },
-    'ubuntu1204' =>
-      { osfamily: 'Debian',
-        release: '12.04',
-        majrelease: '12',
-        lsbdistcodename: 'precise',
-        packages: 'molly_guard' },
-    'ubuntu1404' =>
-      { osfamily: 'Debian',
-        release: '14.04',
-        majrelease: '14',
-        lsbdistcodename: 'trusty',
-        packages: 'molly_guard' },
-    'ubuntu1604' =>
-      { osfamily: 'Debian',
-        release: '16.04',
-        majrelease: '16',
-        lsbdistcodename: 'xenial',
-        packages: 'molly_guard' },
-  }
 
-  describe 'with default values for parameters on' do
-    platforms.sort.each do |k, v|
-      context k.to_s do
-        let :facts do
-          { lsbdistcodename: v[:lsbdistcodename],
-            osfamily: v[:osfamily],
-            kernelrelease: v[:release], # Solaris specific
-            operatingsystemrelease: v[:release], # Linux specific
-            operatingsystemmajrelease: v[:majrelease] }
-        end
+describe 'molly_guard' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) { facts }
 
         it { is_expected.to compile.with_all_deps }
 
         it { is_expected.to contain_class('molly_guard') }
 
-        if v[:packages].class == Array
-          v[:packages].each do |pkg|
-            it do
-              is_expected.to contain_package(pkg).with('ensure' => 'present',
-                                                       'provider' => nil)
-            end
-          end
-        else
-          it do
-            is_expected.to contain_package(v[:packages]).with('ensure' => 'present',
-                                                              'provider' => nil)
-          end
+        it do
+          is_expected.to contain_package('molly_guard').with('ensure'   => 'present',
+							     'name'     => 'molly-guard',
+                                                             'provider' => nil)
         end
 
         it do
           is_expected.to contain_file('/etc/molly-guard/rc').with('ensure' => 'file',
-                                                                  'owner'   => 'root',
-                                                                  'group'   => 'root',
-                                                                  'mode'    => '0644')
+                                                                  'owner'  => 'root',
+                                                                  'group'  => 'root',
+                                                                  'mode'   => '0644')
         end
 
-        molly_guard_rc_fixture = File.read(fixtures("rc.#{k}"))
+        molly_guard_rc_fixture = File.read(fixtures("rc.#{facts[:osfamily]}"))
         it { is_expected.to contain_file('/etc/molly-guard/rc').with_content(molly_guard_rc_fixture) }
+
+        describe 'parameter functionality' do
+          let(:facts) do
+            {
+              osfamily: 'Debian',
+              lsbdistcodename: 'squeeze',
+            }
+          end
+
+          context 'when always_query_hostname is set to valid bool <true>' do
+            let(:params) { { always_query_hostname: true } }
+
+            it { is_expected.to contain_file('/etc/molly-guard/rc').with_content(%r{^ALWAYS_QUERY_HOSTNAME=true$}) }
+          end
+
+          context 'when package_ensure is set to valid string <absent>' do
+            let(:params) { { package_ensure: 'absent' } }
+
+            it { is_expected.to contain_package('molly_guard').with_ensure('absent') }
+          end
+
+          context 'when package_name is set to valid string <molly-guard>' do
+            let(:params) { { package_name: 'molly-guard' } }
+
+            it { is_expected.to contain_package('molly_guard').with_name('molly-guard') }
+          end
+        end
       end
-    end
-  end
-
-  describe 'parameter functionality' do
-    let(:facts) do
-      {
-        osfamily: 'Debian',
-        lsbdistcodename: 'squeeze',
-      }
-    end
-
-    context 'when always_query_hostname is set to valid bool <true>' do
-      let(:params) { { always_query_hostname: true } }
-
-      it { is_expected.to contain_file('/etc/molly-guard/rc').with_content(%r{^ALWAYS_QUERY_HOSTNAME=true$}) }
-    end
-
-    context 'when package_ensure is set to valid string <absent>' do
-      let(:params) { { package_ensure: 'absent' } }
-
-      it { is_expected.to contain_package('molly_guard').with_ensure('absent') }
-    end
-
-    context 'when package_name is set to valid string <molly-guard>' do
-      let(:params) { { package_name: 'molly-guard' } }
-
-      it { is_expected.to contain_package('molly_guard').with_name('molly-guard') }
     end
   end
 
   describe 'failures' do
     let(:facts) do
       {
-        osfamily: 'Debian',
+        osfamily:        'Debian',
         lsbdistcodename: 'squeeze',
       }
     end
 
     context 'when osfamily is unsupported' do
       let :facts do
-        { osfamily: 'Unsupported',
+        { osfamily:                  'Unsupported',
           operatingsystemmajrelease: '9' }
       end
 
@@ -140,10 +82,10 @@ describe 'molly_guard', type: 'class' do
     # set needed custom facts and variables
     let(:facts) do
       {
-        osfamily: 'Debian',
-        operatingsystemrelease: '6.0',
+        osfamily:                  'Debian',
+        operatingsystemrelease:    '6.0',
         operatingsystemmajrelease: '6',
-        lsbdistcodename: 'squeeze',
+        lsbdistcodename:           'squeeze',
       }
     end
     let(:validation_params) do
