d359bf00c20e1067f75dc26ea435a3f104576c7e
Florent Poinsaut <florent@poinsaut.fr>
2018-04-12 15:18:12 +0200

    enable check for puppet 3 and restore compatible with puppet 3

--- Rakefile
+++ Rakefile
@@ -1,4 +1,2 @@
 require 'puppetlabs_spec_helper/rake_tasks'
 require 'puppet-syntax/tasks/puppet-syntax'
-
-PuppetLint.configuration.send('disable_quoted_booleans')
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,23 @@
 # == Class: molly_guard
 #
 class molly_guard (
-  Variant[
-    Boolean,
-    Enum['true', 'false']
-  ] $always_query_hostname = $molly_guard::params::always_query_hostname,
-  String  $package_ensure  = $molly_guard::params::package_ensure,
-  String  $package_name    = $molly_guard::params::package_name,
+  $always_query_hostname = $molly_guard::params::always_query_hostname,
+  $package_ensure        = $molly_guard::params::package_ensure,
+  $package_name          = $molly_guard::params::package_name,
 ) inherits molly_guard::params {
   # <stringified variable handling>
-  $always_query_hostname_bool = $always_query_hostname ? {
-    Boolean => $always_query_hostname,
-    String  => str2bool($always_query_hostname),
-    default => fail('$always_query_hostname parameter is not a boolean'),
+  if is_string($always_query_hostname) {
+    $always_query_hostname_bool = str2bool($always_query_hostname)
+  } else {
+    $always_query_hostname_bool = $always_query_hostname
   }
   # </stringified variable handling>
+
+  # <variable validations>
+  validate_bool($always_query_hostname_bool)
+  validate_string($package_ensure)
+  validate_string($package_name)
+  # </variable validations>
 
   anchor { "${module_name}::begin": }
   -> class { "${module_name}::install": }
